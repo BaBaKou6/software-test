4535ddd0676e4dc1ccb990e5cd3b2cc02ff03dac
nobody <nobody@admin.com>
2024-06-08 15:47:32 +0800

    user model, task model

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from app.models import User, Task
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ("username", )
+    list_display = ("username", "age", "gender", "birthday", "email", "credit", "score", "level", "created")
+    
+    
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
+    list_display = ("name", "user_id", "stage", "score", "created")
+    
+    
--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,48 @@
+from random import choice
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    class Sex(models.TextChoices):
+        M = "male"
+        F = "female"
+        
+    
+    username = models.CharField(max_length=32)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=32, choices=Sex)
+    birthday = models.DateField()
+    email = models.EmailField()
+    
+    credit = models.IntegerField()
+    score = models.IntegerField()
+    level = models.IntegerField()
+    
+    created = models.DateField()
+    last_modified = models.DateField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = "用户"
+        
+        
+class Task(models.Model):
+    class TaskStage(models.TextChoices):
+        init = "male"
+        doing = "female"
+        finished = "finished"
+        cancelled = "cancelled"
+        
+    name = models.CharField(max_length=64)
+    user_id = models.IntegerField()
+    stage = models.CharField(max_length = 32, choices=TaskStage)
+    score = models.IntegerField()
+    
+      
+    created = models.DateField()
+    last_modified = models.DateField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "任务"
+        verbose_name_plural = "任务"
